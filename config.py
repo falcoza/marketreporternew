90367a03b38747981e469cd0bed51f6f294db2cf
falcoza <ypanchia@gmail.com>
2025-06-26 17:12:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ EMAIL_RECEIVERS = [
     "yeshiel@dailymaverick.co.za",
     "neesa@dailymaverick.co.za",
     "lisakanya@dailymaverick.co.za",
+    "kara@dailymaverick.co.za",
     "lindsey@dailymaverick.co.za"
 ]
 SMTP_SERVER = "smtp.gmail.com"
