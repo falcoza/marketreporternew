602651a24778b3c53ebf3c1b027025dcba15cd6f
falcoza <ypanchia@gmail.com>
2025-08-07 07:39:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,7 @@ from datetime import datetime
 EMAIL_SENDER = "yeshiel@dailymaverick.co.za"
 EMAIL_PASSWORD = os.getenv("MarketReport")
 EMAIL_RECEIVERS = [
-    "yeshiel@dailymaverick.co.za",
-    "neesa@dailymaverick.co.za",
-    "lisakanya@dailymaverick.co.za",
-    "kara@dailymaverick.co.za",
-    "lindsey@dailymaverick.co.za",
-    "bm@dailymaverick.co.za"
+    "yeshiel@dailymaverick.co.za"
 ]
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
